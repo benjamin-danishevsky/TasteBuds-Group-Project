805e46246cba95827971b85a348babe02cdfa2b0
James Chen <jameschen09@yahoo.com>
2022-05-03 12:25:01 -0700

    basic css for individual groups until we get more features done

--- react-app/src/components/SingleGroup/index.js
+++ react-app/src/components/SingleGroup/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import * as groupActions from '../../store/groups'
+import './SingleGroup.css'
 
 const SingleGroup = () => {
 
@@ -23,11 +24,23 @@ const SingleGroup = () => {
   return (
     <>
       <div>
-        <h1>{groups?.name}</h1>
+        <div className="group_header">
+        {console.log(groups)}
+        <div className="header_left">
+          <img className="header_img" src={groups?.background_img} alt={groups?.name}/>
+        </div>
+        <div className="header_right">
+          <h1>{groups?.name}</h1>
+          <p>{groups?.city}, {groups?.state}</p>
+        </div>
+      </div>
         <button type="button" onClick={()=> {
           onClick(groups.id)
         }}>Delete</button>
+      <div>
+        {groups?.description}
       </div>
+    </div>
     </>
   )
 }
