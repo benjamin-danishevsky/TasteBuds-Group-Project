9251966dc27a0be00b85115eddb599b87a689d3f
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-05-04 19:08:34 -0400

    back end search bar working with multiple words

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -11,29 +11,34 @@ def search():
 
     if request.method == 'POST':
         info = request.json # { "search": "tennis san francisco" }
+        info_list = info['search'].split(' ')
 
-        #events back from search by location
-        events_location = Event.query.filter(Event.location.ilike(f"%{info['search']}%")).all() #working
-        # return {
-        #     "events": [event.to_dict() for event in events]
-        # }
+        final_events_list = set()
+        for word in info_list:
 
-        # all events back from search by group name
-        groups = Group.query.filter(Group.name.ilike(f"%{info['search']}%")).all()
-        new_list_group_name = sum([group.groups for group in groups], [])
-        # return {
-        #     'events': [event.to_dict() for event in new_list]
-        # }
+            #events back from search by location
+            events_location = Event.query.filter(Event.location.ilike(f"%{word}%")).all() #working
+            # return {
+            #     "events": [event.to_dict() for event in events]
+            # }
 
-        #all events back from search by group description
-        groups2 = Group.query.filter(Group.description.ilike(f"%{info['search']}%")).all()
-        new_list_group_description = sum([group.groups for group in groups2], [])
+            # all events back from search by group name
+            groups = Group.query.filter(Group.name.ilike(f"%{word}%")).all()
+            new_list_group_name = sum([group.groups for group in groups], [])
+            # return {
+            #     'events': [event.to_dict() for event in new_list]
+            # }
+
+            #all events back from search by group description
+            groups2 = Group.query.filter(Group.description.ilike(f"%{word}%")).all()
+            new_list_group_description = sum([group.groups for group in groups2], [])
 
 
-        print(info,'------'*50)
-        final_multi_list = [events_location, new_list_group_description, new_list_group_name]
-        result = set().union(*final_multi_list)
+            final_multi_list = [events_location, new_list_group_description, new_list_group_name]
+            result = set().union(*final_multi_list)
+            final_events_list.update(result)
+
 
         return {
-            'events': [event.to_dict() for event in result]
+            'events': [event.to_dict() for event in final_events_list]
         }
