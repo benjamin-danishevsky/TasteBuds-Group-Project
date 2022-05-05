1e16f1c4b4cd20dee7dd810640a28248231b69df
James Chen <jameschen09@yahoo.com>
2022-05-05 10:58:41 -0700

    added search bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -4,33 +4,41 @@ import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import LoginFormModal from './auth/LoginModal';
 import SignUpFormModal from './auth/SignUpModal';
+import SearchBar from './SearchBar';
 import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-        <li>
-          <LoginFormModal />
-        </li>
-        <li>
-          <SignUpFormModal />
-        </li>
-      </ul>
-    </nav>
+    <div className='nav-bar'>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <div className='search-bar'>
+            <SearchBar />
+          </div>
+          <div className='auth-routes'>
+            <li>
+              <LogoutButton />
+            </li>
+            <li>
+              <LoginFormModal />
+            </li>
+            <li>
+              <SignUpFormModal />
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
