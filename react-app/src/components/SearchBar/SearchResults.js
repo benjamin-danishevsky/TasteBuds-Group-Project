8fc598a77472dfac0a0bbb9766ea898e00e1b531
grant <grntwalton@gmail.com>
2022-05-06 23:33:45 -0700

    minor changes

--- react-app/src/components/SearchBar/SearchResults.js
+++ react-app/src/components/SearchBar/SearchResults.js
@@ -25,7 +25,6 @@ const SearchResults = () => {
                             <li>{event?.location}</li>
                             <li>{event?.date}</li>
                             <img src={event?.background_img}/>
-                            <button>JOIN/LEAVE</button>
                         </ul>
                     </a>
                 ))}
