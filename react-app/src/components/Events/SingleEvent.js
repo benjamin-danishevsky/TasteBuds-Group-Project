02035e2a029215d0d8385112e4bed45dfe91ee48
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-05-07 17:10:06 -0400

    added create new group button to groups page

--- react-app/src/components/Events/SingleEvent.js
+++ react-app/src/components/Events/SingleEvent.js
@@ -25,7 +25,6 @@ const SingleEvent = () => {
         if(!sessionUser) {
             setVisibility(false)
         }
-
     }, [])
 
     useEffect(() => {
--- react-app/src/components/Groups/index.js
+++ react-app/src/components/Groups/index.js
@@ -2,41 +2,60 @@ import React, {useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as groupActions from '../../store/groups'
 import './Groups.css'
+import { useHistory } from "react-router-dom";
+import { motion } from 'framer-motion';
 
 const Groups = () => {
 
   const dispatch = useDispatch()
+  const history = useHistory()
+  const [visibility, setVisibility] = useState(true)
 
   useEffect(async () => {
     await dispatch(groupActions.loadAllGroupsThunk())
   }, [dispatch])
 
+  useEffect(() => {
+    if(!sessionUser) {
+        setVisibility(false)
+    }
+}, [])
+
+  const sessionUser = useSelector(state => state.session.user)
+  console.log(sessionUser)
   const groups = useSelector(state => state.groups);
   const groupData = Object.values(groups)
 
   return (
     <>
-    <div>
-      {groupData.map((group, idx) => (
-        <>
-        <div className ="allGroups" key={idx}>
-          <a href={`/groups/${group.id}`}>
-            <ul>
-              <div className="allGroupsImg">
-                <img src={group.background_img} alt={group.name} />
-              </div>
-              <div className="allGroupsInfo">
-                  <span style={{fontWeight: "bold", fontSize: "30px", marginBottom: "20px"}}><li>{group?.name}</li></span>
-                <li>{group?.city}, {group?.state}</li>
-                <li>{group?.location}</li>
-                <li>{group?.description}</li>
-              </div>
-            </ul>
-          </a>
-        </div>
-        </>
-      ))}
-    </div>
+      <div>
+        {groupData.map((group, idx) => (
+          <>
+            <div className ="allGroups" key={idx}>
+              <a href={`/groups/${group.id}`}>
+                <ul>
+                  <div className="allGroupsImg">
+                    <img src={group.background_img} alt={group.name} />
+                  </div>
+                  <div className="allGroupsInfo">
+                      <span style={{fontWeight: "bold", fontSize: "30px", marginBottom: "20px"}}><li>{group?.name}</li></span>
+                    <li>{group?.city}, {group?.state}</li>
+                    <li>{group?.location}</li>
+                    <li>{group?.description}</li>
+                  </div>
+                </ul>
+              </a>
+            </div>
+          </>
+        ))}
+      </div>
+      <motion.button
+        style={{  backgroundColor: '#ff5607', border: 'none', borderRadius:'20px', height:'2.8em', width:'8rem', color:'white', marginLeft: '5px', visibility : visibility ? 'visible' : 'hidden'}}
+        onClick={() => history.push('/groups/new-group')}
+        whileHover={{scale: 1.1}}
+        whileTap={{scale: .9}}
+        >
+          Create New Group</motion.button>
     </>
   )
 }
